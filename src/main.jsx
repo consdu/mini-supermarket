0948c566427f486cea34dd681b5b9f44b7bbb6c4
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-08-31 16:29:26 +0200

    Move appRouter into its own folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { SWRConfig } from "swr";
+import { RouterProvider } from "react-router-dom";
 import CartProvider from "./contexts/CartContext/CartContextProvider";
-import App from "./components//App/App";
+import router from "./routers/appRouter/appRouter";
 import "./styles.css";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
@@ -11,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SWRConfig value={{ fetcher }}>
       <CartProvider>
-        <App />
+        <RouterProvider router={router} />
       </CartProvider>
     </SWRConfig>
   </React.StrictMode>
