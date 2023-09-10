438b1fcae8b768a165ab2c0b286b8cb3f7119db4
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-09-10 23:10:02 +0200

    Add dark mode support with button to main navigation bar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,29 @@
 import { NavLink } from "react-router-dom";
 import useCart from "../../hooks/useCart/useCart";
+import { useEffect, useState } from "react";
+import Button from "../Button/Button";
 
 const Navbar = (): React.ReactElement => {
+  const [darkTheme, setDarkTheme] = useState(
+    () => localStorage.getItem("theme") === "dark" || false
+  );
+
   const { cart } = useCart();
+
   const cartCount = cart.reduce(
     (total, product) => total + product.quantity,
     0
   );
+
+  useEffect(() => {
+    localStorage.setItem("theme", darkTheme ? "dark" : "light");
+  }, [darkTheme]);
+
+  useEffect(() => {
+    darkTheme
+      ? document.querySelector("body")!.classList.add("dark")
+      : document.querySelector("body")!.classList.remove("dark");
+  });
 
   return (
     <nav className="navbar">
@@ -14,6 +31,15 @@ const Navbar = (): React.ReactElement => {
         SuperMarket
       </NavLink>
       <ul>
+        <li className="nav-item">
+          <Button
+            className="theme-switcher"
+            onClick={() => setDarkTheme(!darkTheme)}
+            type="button"
+          >
+            {darkTheme ? "Dark" : "Light"}
+          </Button>
+        </li>
         <li className="nav-item">
           <NavLink
             end
